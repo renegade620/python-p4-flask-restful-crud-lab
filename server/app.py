a7e7a07cb2d4e2bbba0f6e23b4c0fc3cb992dcb3
Frankline Were <franklinegift@gmail.com>
2024-10-10 22:11:45 +0300

    patch & delete requests

--- server/app.py
+++ server/app.py
@@ -46,6 +46,32 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+        plant_record = Plant.query.filter_by(id=id).first()
+        for attr in data:
+            setattr(plant_record, attr, data[attr])
+
+        db.session.add(plant_record)
+        db.session.commit()
+
+        plant = plant_record.to_dict()
+
+        response = make_response(jsonify(plant), 200)
+
+        return response
+    
+    def delete(self, id):
+        plant_record = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant_record)
+        db.session.commit()
+
+        plant = {"message": ""}
+
+        response = make_response(plant, 204)
+        return response
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
